284ce3e3fec63ac016734ac1b246f2b0cfbfaf3b
EDWIN JOHN NADARAJAN <edwinj@deakin.edu.au>
2020-10-14 14:36:41 +1100

    Contract permissions set

--- backend/contracts/QuizMaster.sol
+++ backend/contracts/QuizMaster.sol
@@ -1,22 +1,21 @@
-
 pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
 
 contract QuizMaster{
     mapping(address=>uint) public time;
     mapping(address=>uint) public marks;
-    // time public dateTime;
     address public system;
     mapping(address=>uint256) public quizCount;
     mapping(address=>uint) questionCount;
     mapping(address=>mapping(uint=>string)) public questions;
     mapping(address=>mapping(uint=>uint)) public weightage;
-    mapping(address=>mapping(uint=>string)) public answers;
+    mapping(address=>mapping(uint=>string)) answers;
     
     constructor() public{
         system = msg.sender;
     }
     function startQuiz(address _quizzer, uint _time, string[] memory _questions, string[] memory _answers, uint[] memory _weights) public{
+        require(msg.sender==system);
         time[_quizzer] = _time;
         marks[_quizzer] = 0;
         questionCount[_quizzer] = _answers.length-1;
@@ -28,6 +27,7 @@ contract QuizMaster{
         quizCount[_quizzer]++;
     }
     function checkResult(address _quizzer, string[] memory _answers) public returns(uint, uint){
+        require(msg.sender==system);
         marks[_quizzer] = 0;
         for(uint i=0;i<=questionCount[_quizzer];i++){
             if(keccak256(abi.encodePacked((answers[_quizzer][i]))) == keccak256(abi.encodePacked((_answers[i])))){
